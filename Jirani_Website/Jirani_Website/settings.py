b5d20b00ef4c71cce5992935e22d335e6c52e60f
Victorcoder254 <144835344+Victorcoder254@users.noreply.github.com>
2024-03-12 10:25:52 +0300

    Update settings.py

--- Jirani_Website/Jirani_Website/settings.py
+++ Jirani_Website/Jirani_Website/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','192.168.59.201', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
